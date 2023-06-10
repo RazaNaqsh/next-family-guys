4945294679cc660619fa654aff3806c9a72a8211
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-10 11:41:26 +0530

    Navigation bar

---
+++ .next/static/webpack/app/page.5dc27e2bff1b48eb.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fhome%2Fraza%2Frepos%2FfullstackProjects%2Fnext-family-guy%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&modules=%2Fhome%2Fraza%2Frepos%2FfullstackProjects%2Fnext-family-guy%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&server=false!":
+/*!**************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fhome%2Fraza%2Frepos%2FfullstackProjects%2Fnext-family-guy%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&modules=%2Fhome%2Fraza%2Frepos%2FfullstackProjects%2Fnext-family-guy%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&server=false! ***!
+  \**************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/link.js */ \"(app-client)/./node_modules/next/dist/client/link.js\", 23));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/image.js */ \"(app-client)/./node_modules/next/dist/client/image.js\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRmhvbWUlMkZyYXphJTJGcmVwb3MlMkZmdWxsc3RhY2tQcm9qZWN0cyUyRm5leHQtZmFtaWx5LWd1eSUyRm5vZGVfbW9kdWxlcyUyRm5leHQlMkZkaXN0JTJGY2xpZW50JTJGbGluay5qcyZtb2R1bGVzPSUyRmhvbWUlMkZyYXphJTJGcmVwb3MlMkZmdWxsc3RhY2tQcm9qZWN0cyUyRm5leHQtZmFtaWx5LWd1eSUyRm5vZGVfbW9kdWxlcyUyRm5leHQlMkZkaXN0JTJGY2xpZW50JTJGaW1hZ2UuanMmc2VydmVyPWZhbHNlIS5qcyIsIm1hcHBpbmdzIjoiQUFBQSx1TUFBNkg7QUFDN0giLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz8zMmUzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiL2hvbWUvcmF6YS9yZXBvcy9mdWxsc3RhY2tQcm9qZWN0cy9uZXh0LWZhbWlseS1ndXkvbm9kZV9tb2R1bGVzL25leHQvZGlzdC9jbGllbnQvbGluay5qc1wiKTtcbmltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiL2hvbWUvcmF6YS9yZXBvcy9mdWxsc3RhY2tQcm9qZWN0cy9uZXh0LWZhbWlseS1ndXkvbm9kZV9tb2R1bGVzL25leHQvZGlzdC9jbGllbnQvaW1hZ2UuanNcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fhome%2Fraza%2Frepos%2FfullstackProjects%2Fnext-family-guy%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&modules=%2Fhome%2Fraza%2Frepos%2FfullstackProjects%2Fnext-family-guy%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&server=false!\n"));
+
+/***/ })
+
+});
